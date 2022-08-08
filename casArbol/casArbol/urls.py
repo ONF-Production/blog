116a184315ef4451ff0c329d8a4ef80a3a3a8c1b
ONF <onf_production@outlook.es>
2022-08-08 11:02:24 -0300

    creacion de app noticia

--- casArbol/casArbol/urls.py
+++ casArbol/casArbol/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.noticia.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('noticia/', include('apps.noticia.urls'), name='noticia'),
 ]
