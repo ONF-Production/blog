1f7abdcae6ca1c23e269d08d2e7861481cc9c0e8
SanRRuiz <sandrarosanaruiz3@gmail.com>
2022-08-21 16:45:40 -0300

    modelos y db

--- casArbol/apps/noticia/admin.py
+++ casArbol/apps/noticia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Noticia, Comentario, Evento
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Noticia)
+admin.site.register(Comentario)
+admin.site.register(Evento)
--- casArbol/casArbol/settings/local.py
+++ casArbol/casArbol/settings/local.py
@@ -5,8 +5,12 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_casArbol',
+        'USER': 'root',
+        'PASSWORD': 'sandra4321',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
