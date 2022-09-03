fa6956ab3add0c282e762f5350183d082b8f79a5
Nestor677 <103796417+Nestor677@users.noreply.github.com>
2022-09-03 06:22:39 -0300

    configuración de heroku

--- casArbol/casArbol/settings/base.py
+++ casArbol/casArbol/settings/base.py
@@ -139,3 +139,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL ='/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+                                   
